663da87d16fa0cc772c4f7046256b5475a08e648
braunl <braunl@inf.ethz.ch>
2016-05-24 14:31:26 +0200

    worked on MT annotation, for the sample queries everything works now

--- src/MtLib.hs
+++ src/MtLib.hs
@@ -56,9 +56,6 @@ mtSchemaSpecFromList = M.fromList
 mtSpecificTableFromList :: [(MtAttributeName, MtAttributeComparability)] -> MtSpecificTable
 mtSpecificTableFromList = M.fromList
 
-mtGetTenantIdentifier :: MtTableName -> String
-mtGetTenantIdentifier s = head s : "_TENANT_KEY"
-
 -- ##################################
 -- Parsing and Printing
 -- ##################################
@@ -94,48 +91,77 @@ mtRewrite spec config query = do
         )
         annotatedQuery
 
+mtGetTenantIdentifier :: MtTableName -> String
+mtGetTenantIdentifier s = head s : "_TENANT_KEY"
+
 mtRewriteSelectList :: MtSchemaSpec -> (MtClient, MtDataSet) -> Pa.TableRefList -> Pa.SelectList -> Pa.SelectList
 mtRewriteSelectList spec config tref (Pa.SelectList ann items) = Pa.SelectList ann (concatMap (mtRewriteSelectItem spec config tref) items)
 
 mtRewriteSelectItem :: MtSchemaSpec -> (MtClient, MtDataSet) -> Pa.TableRefList -> Pa.SelectItem -> [Pa.SelectItem]
--- replaces Star Expressions with an enumeration of all attributes instead (* would also display tenant key, which is something we do not want)
-mtRewriteSelectItem spec config [Pa.Tref tAnn (Pa.Name nameAnn [Pa.Nmc tname])] (Pa.SelExp selAnn (Pa.Star starAnn)) =
-    let tableSpec = M.lookup tname spec
-        generate (Just (FromMtSpecificTable tab)) = map (Pa.SelExp selAnn . Pa.StringLit starAnn) (M.keys tab)
-        generate _ = [Pa.SelExp selAnn (Pa.Star starAnn)]
-    in generate tableSpec
 -- replace transformable function
 -- TODO: continue here
 -- default case
 mtRewriteSelectItem spec config tref item = [item]
+
+-- ##################################
+-- MT Annotate
+-- ##################################
 
 -- make sure that every attribute of a specific table appears with its table name
 -- assumes that attributes not found within the schema are from global tables
 mtAnnotate :: MtSchemaSpec -> Pa.QueryExpr -> Pa.QueryExpr
 mtAnnotate spec (Pa.Select ann selDistinct selSelectList selTref selWhere
     selGroupBy selHaving selOrderBy selLimit selOffset selOption) =
-        let rewrittenList = mtAnnotateSelectList spec selTref selSelectList
-        in  Pa.Select ann selDistinct rewrittenList selTref selWhere
-            selGroupBy selHaving selOrderBy selLimit selOffset selOption
+        let newSelectList = mtAnnotateSelectList spec selTref selSelectList
+            newTrefs = mtAnnotateTrefList spec selTref
+            newWhere = mtAnnotateMaybeScalarExpr spec selTref selWhere
+            newGroupBy = map (mtAnnotateScalarExpr spec selTref) selGroupBy
+        in  Pa.Select ann selDistinct newSelectList newTrefs newWhere
+            newGroupBy selHaving selOrderBy selLimit selOffset selOption
+-- default case handles anything we do not handle so far
+mtAnnotate _ query = query
+
+mtAnnotateTrefList :: MtSchemaSpec -> Pa.TableRefList -> Pa.TableRefList
+mtAnnotateTrefList spec (Pa.SubTref ann sel:trefs) = Pa.SubTref ann (mtAnnotate spec sel) : mtAnnotateTrefList spec trefs
+mtAnnotateTrefList spec (tref:trefs) = tref:mtAnnotateTrefList spec trefs
+mtAnnotateTrefList _ [] = []
 
 mtAnnotateSelectList :: MtSchemaSpec -> Pa.TableRefList -> Pa.SelectList -> Pa.SelectList
-mtAnnotateSelectList spec trefs (Pa.SelectList ann (item:items)) = Pa.SelectList ann ((mtAnnotateSelectItem spec trefs item):items)
+mtAnnotateSelectList spec trefs (Pa.SelectList ann items) = Pa.SelectList ann (mtAnnotateSelectItems spec trefs items)
 
+mtAnnotateSelectItems :: MtSchemaSpec -> Pa.TableRefList -> [Pa.SelectItem] -> [Pa.SelectItem]
+-- replaces Star Expressions with an enumeration of all attributes instead (* would also display tenant key, which is something we do not want)
+mtAnnotateSelectItems spec [Pa.Tref tAnn (Pa.Name nameAnn [Pa.Nmc tname])] [Pa.SelExp selAnn (Pa.Star starAnn)] =
+    let tableSpec = M.lookup tname spec
+        generate (Just (FromMtSpecificTable tab)) = map (\key -> Pa.SelExp selAnn (Pa.Identifier starAnn (Pa.Name starAnn [Pa.Nmc key]))) (M.keys tab)
+        generate _ = [Pa.SelExp selAnn (Pa.Star starAnn)]
+    in mtAnnotateSelectItems spec [Pa.Tref tAnn (Pa.Name nameAnn [Pa.Nmc tname])] (generate tableSpec)
+-- default case, recursively call annotation call on single item
+mtAnnotateSelectItems _ _ [] = []
+mtAnnotateSelectItems spec trefs (item:items) = mtAnnotateSelectItem spec trefs item : mtAnnotateSelectItems spec trefs items
 mtAnnotateSelectItem :: MtSchemaSpec -> Pa.TableRefList -> Pa.SelectItem -> Pa.SelectItem
 mtAnnotateSelectItem spec trefs (Pa.SelExp ann scalExp)             = Pa.SelExp ann (mtAnnotateScalarExpr spec trefs scalExp)
 mtAnnotateSelectItem spec trefs (Pa.SelectItem ann scalExp newName) = Pa.SelectItem ann (mtAnnotateScalarExpr spec trefs scalExp) newName
 
+mtAnnotateMaybeScalarExpr :: MtSchemaSpec -> Pa.TableRefList -> Maybe Pa.ScalarExpr -> Maybe Pa.ScalarExpr
+mtAnnotateMaybeScalarExpr spec trefs (Just expr) = Just (mtAnnotateScalarExpr spec trefs expr)
+mtAnnotateMaybeScalarExpr _ _ Nothing = Nothing
+
 mtAnnotateScalarExpr :: MtSchemaSpec -> Pa.TableRefList -> Pa.ScalarExpr -> Pa.ScalarExpr
-mtAnnotateScalarExpr spec ((Pa.Tref tAnn (Pa.Name nameAnn [Pa.Nmc tname])):trefs) (Pa.Identifier iAnn (Pa.Name a ((Pa.Nmc attName):nameComps)))
-    | length nameComps > 0  = Pa.Identifier iAnn (Pa.Name a ((Pa.Nmc attName):nameComps))
+mtAnnotateScalarExpr spec treflist (Pa.App ann funName args) = Pa.App ann funName (map (mtAnnotateScalarExpr spec treflist) args)
+mtAnnotateScalarExpr spec treflist (Pa.BinaryOp ann opName arg0 arg1) = Pa.BinaryOp ann opName
+        (mtAnnotateScalarExpr spec treflist arg0) (mtAnnotateScalarExpr spec treflist arg1)
+mtAnnotateScalarExpr spec treflist (Pa.SpecialOp ann opName args) = Pa.SpecialOp ann opName (map (mtAnnotateScalarExpr spec treflist) args)
+mtAnnotateScalarExpr spec (Pa.Tref _ (Pa.Name _ [Pa.Nmc tname]):trefs) (Pa.Identifier iAnn (Pa.Name a (Pa.Nmc attName:nameComps)))
+    | not (null nameComps)  = Pa.Identifier iAnn (Pa.Name a (Pa.Nmc attName : nameComps))
     | otherwise             =
         let tableSpec = M.lookup tname spec
             containsAttribute (Just (FromMtSpecificTable attrMap)) aName = M.member aName attrMap 
             containsAttribute _ _ = False
             contains = containsAttribute tableSpec attName
             propagate False = mtAnnotateScalarExpr spec trefs (Pa.Identifier iAnn (Pa.Name a [Pa.Nmc attName]))
-            propagate True  = Pa.Identifier iAnn (Pa.Name a [(Pa.Nmc tname),(Pa.Nmc attName)])
+            propagate True  = Pa.Identifier iAnn (Pa.Name a [Pa.Nmc tname,Pa.Nmc attName])
         in  propagate contains
+-- default case handles anything we do not handle so far
 mtAnnotateScalarExpr _ _ expr = expr
 
--- TODO: continue here
