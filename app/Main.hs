47edd6542c4e84405649fb48e33c22bf1d91861e
braunl <braunl@inf.ethz.ch>
2016-06-16 16:35:54 +0200

    started adding tpch queries one by one

--- app/Main.hs
+++ app/Main.hs
@@ -2,84 +2,77 @@ module Main where
 
 import MtLib
 
-generateSupplierTable :: MtSpecificTable
-generateSupplierTable = mtSpecificTableFromList
-    [("S_SUPPKEY", MtSpecific)
-    ,("S_NAME", MtComparable)
-    ,("S_ADDRESS", MtComparable)
-    ,("S_NATIONKEY", MtComparable)
-    ,("S_PHONE", MtTransformable "phoneToUniversal" "phoneFromUniversal")
-    ,("S_ACCTBAL", MtTransformable "currencyToUniversal" "currencyFromUniversal")
-    ,("S_COMMENT", MtComparable)
-    ]
 generateCustomerTable :: MtSpecificTable
 generateCustomerTable = mtSpecificTableFromList
-    [("C_CUSTKEY", MtSpecific)
-    ,("C_NAME", MtComparable)
-    ,("C_ADDRESS", MtComparable)
-    ,("C_NATIONKEY", MtComparable)
-    ,("C_PHONE", MtTransformable "phoneToUniversal" "phoneFromUniversal")
-    ,("C_ACCTBAL", MtTransformable "currencyToUniversal" "currencyFromUniversal")
-    ,("C_MKTSEGMENT", MtComparable)
-    ,("C_COMMENT", MtComparable)
+    [("C_custkey"   , MtSpecific)
+    ,("C_name"      , MtComparable)
+    ,("C_address"   , MtComparable)
+    ,("C_nationkey" , MtComparable)
+    ,("C_phone"     , MtTransformable "phoneToUniversal" "phoneFromUniversal")
+    ,("C_acctbal"   , MtTransformable "currencyToUniversal" "currencyFromUniversal")
+    ,("C_mktsegment", MtComparable)
+    ,("C_comment"   , MtComparable)
     ]
 
 generateOrdersTable :: MtSpecificTable
 generateOrdersTable = mtSpecificTableFromList
-    [("O_ORDERKEY", MtSpecific)
-    ,("O_CUSTKEY", MtSpecific)
-    ,("O_ORDERSTATUS", MtComparable)
-    ,("O_TOTALPRICE", MtTransformable "currencyToUniversal" "currencyFromUniversal")
-    ,("O_ORDERDATE", MtComparable)
-    ,("O_ORDERPRIORITY", MtComparable)
-    ,("O_CLERK", MtComparable)
-    ,("O_SHIPPRIORITY", MtComparable)
-    ,("O_COMMENT", MtComparable)
+    [("O_orderkey"      , MtSpecific)
+    ,("O_custkey"       , MtSpecific)
+    ,("O_orderstatus"   , MtComparable)
+    ,("O_totalprice"    , MtTransformable "currencyToUniversal" "currencyFromUniversal")
+    ,("O_orderdate"     , MtComparable)
+    ,("O_orderpriority" , MtComparable)
+    ,("O_clerk"         , MtComparable)
+    ,("O_shippriority"  , MtComparable)
+    ,("O_comment"       , MtComparable)
     ]
 
 generateLineitemTable :: MtSpecificTable
 generateLineitemTable = mtSpecificTableFromList
-  [("L_ORDERKEY", MtSpecific)
-  ,("L_SUPPKEY", MtSpecific)
-  ,("L_LINENUMBER", MtComparable)
-  ,("L_QUANTITY", MtComparable)
-  ,("L_EXTENDEDPRICE", MtTransformable "currencyToUniversal" "currencyFromUniversal")
-  ,("L_DISCOUNT", MtComparable)
-  ,("L_TAX", MtComparable)
-  ,("L_RETURNFLAG", MtComparable)
-  ,("L_LINESTATUS", MtComparable)
-  ,("L_SHIPDATE", MtComparable)
-  ,("L_COMMITDATE", MtComparable)
-  ,("L_RECEIPTDATE", MtComparable)
-  ,("L_SHIPINSTRUCT", MtComparable)
-  ,("L_SHIPMODE", MtComparable)
-  ,("L_COMMENT", MtComparable)
+  [("L_orderkey"        , MtSpecific)
+  ,("L_partkey"         , MtComparable)
+  ,("L_suppkey"         , MtComparable)
+  ,("L_linenumber"      , MtComparable)
+  ,("L_quantity"        , MtComparable)
+  ,("L_extendedprice"   , MtTransformable "currencyToUniversal" "currencyFromUniversal")
+  ,("L_discount"        , MtComparable)
+  ,("L_tax"             , MtComparable)
+  ,("L_returnflag"      , MtComparable)
+  ,("L_linestatus"      , MtComparable)
+  ,("L_shipdate"        , MtComparable)
+  ,("L_commitdate"      , MtComparable)
+  ,("L_receiptdate"     , MtComparable)
+  ,("L_shipinstruct"    , MtComparable)
+  ,("L_shipmode"        , MtComparable)
+  ,("L_comment"         , MtComparable)
   ]
 
 generateTestSchema :: MtSchemaSpec
 generateTestSchema = mtSchemaSpecFromList
-    [("REGION", MtGlobalTable)
-    ,("NATION", MtGlobalTable)
-    ,("CUSTOMER", FromMtSpecificTable generateCustomerTable)
-    ,("ORDERS", FromMtSpecificTable generateOrdersTable)
-    ,("SUPPLIER", FromMtSpecificTable generateSupplierTable)
-    ,("LINEITEM", FromMtSpecificTable generateLineitemTable)
+    [("Region", MtGlobalTable)
+    ,("Nation", MtGlobalTable)
+    ,("Part", MtGlobalTable)
+    ,("Supplier", MtGlobalTable)
+    ,("Partsupp", MtGlobalTable)
+    ,("Customer", FromMtSpecificTable generateCustomerTable)
+    ,("Orders", FromMtSpecificTable generateOrdersTable)
+    ,("Lineitem", FromMtSpecificTable generateLineitemTable)
     ]
 
 
 runTestQueries :: MtSchemaSpec -> MtSetting -> IO ()
 runTestQueries spec setting = do
-    let queries = ["SELECT * FROM SUPPLIER;"
-                   ,"SELECT * FROM SUPPLIER WHERE S_NAME = ?;"
-                   ,"SELECT DISTINCT S_NAME FROM SUPPLIER WHERE S_ACCTBAL > 42;"
-                   ,"SELECT S.S_NAME as SUP_NAME, N.N_NAME as NAT_NAME FROM SUPPLIER S, NATION N WHERE S.S_NATIONKEY = N.N_NATIONKEY"
-                   ,"SELECT max(S_ACCT) as NATIONMAX FROM (SELECT avg(S_ACCTBAL) as S_ACCT FROM SUPPLIER GROUP BY S_NATIONKEY ORDER BY S_NATIONKEY) tmp"
-                   ,"SELECT SUM(L_EXTENDEDPRICE*L_DISCOUNT) AS REVENUE FROM LINEITEM WHERE L_SHIPDATE >= '1994-01-01' AND L_SHIPDATE < cast('1995-01-01' as date) AND L_DISCOUNT BETWEEN .06 - 0.01 AND .06 + 0.01 AND L_QUANTITY < 24" -- Q6
-                   ,"SELECT C_CUSTKEY, C_NAME, SUM(L_EXTENDEDPRICE*(1-L_DISCOUNT)) AS REVENUE, C_ACCTBAL, N_NAME, C_ADDRESS, C_PHONE, C_COMMENT FROM CUSTOMER, ORDERS, LINEITEM, NATION WHERE C_CUSTKEY = O_CUSTKEY AND L_ORDERKEY = O_ORDERKEY AND O_ORDERDATE>= '1993-10-01' AND O_ORDERDATE < cast('1994-01-01' as date) AND L_RETURNFLAG = 'R' AND C_NATIONKEY = N_NATIONKEY GROUP BY C_CUSTKEY, C_NAME, C_ACCTBAL, C_PHONE, N_NAME, C_ADDRESS, C_COMMENT ORDER BY REVENUE DESC LIMIT 20" -- Q10
-                   ,"SELECT CNTRYCODE, COUNT(*) AS NUMCUST, SUM(C_ACCTBAL) AS TOTACCTBAL FROM (SELECT SUBSTRING(C_PHONE,1,2) AS CNTRYCODE, C_ACCTBAL FROM CUSTOMER WHERE SUBSTRING(C_PHONE,1,2) IN ('13', '31', '23', '29', '30', '18', '17') AND C_ACCTBAL > (SELECT AVG(C_ACCTBAL) FROM CUSTOMER WHERE C_ACCTBAL > 0.00 AND SUBSTRING(C_PHONE,1,2) IN ('13', '31', '23', '29', '30', '18', '17')) AND NOT EXISTS ( SELECT * FROM ORDERS WHERE O_CUSTKEY = C_CUSTKEY)) AS CUSTSALE GROUP BY CNTRYCODE ORDER BY CNTRYCODE" -- Q22
-                   ,"SELECT C.C_CUSTKEY, C.C_NAME, SUM(L.L_EXTENDEDPRICE*(1-L.L_DISCOUNT)) AS REVENUE, C.C_ACCTBAL, N.N_NAME, C.C_ADDRESS, C.C_PHONE, C.C_COMMENT FROM CUSTOMER C, ORDERS O, LINEITEM L, NATION N WHERE C.C_CUSTKEY = O.O_CUSTKEY AND L.L_ORDERKEY = O.O_ORDERKEY AND O.O_ORDERDATE>= '1993-10-01' AND O.O_ORDERDATE < cast('1994-01-01' as date) AND L.L_RETURNFLAG = 'R' AND C.C_NATIONKEY = N.N_NATIONKEY GROUP BY C.C_CUSTKEY, C.C_NAME, C.C_ACCTBAL, C.C_PHONE, N.N_NAME, C.C_ADDRESS, C.C_COMMENT ORDER BY REVENUE DESC LIMIT 20" -- Q10, with tables renamed
-                   ,"SELECT C_NAME, N_NATIONKEY FROM CUSTOMER, NATION WHERE C_CUSTKEY = N_NATIONKEY"
-                   ,"SELECT C_NAME, O_ORDERKEY FROM CUSTOMER, ORDERS WHERE O_TOTALPRICE = C_CUSTKEY "
+    let queries = ["SELECT * FROM Supplier;"
+                    ,"SELECT * FROM Supplier WHERE S_name = ?;"
+                    ,"SELECT DISTINCT S_name FROM Supplier WHERE S_acctbal > 42;"
+                    ,"SELECT S.S_name as SUP_NAME, N.n_name as NAT_NAME FROM Supplier S, Nation N WHERE S.s_nationkey = N.n_nationkey"
+                    ,"SELECT max(S_acct) as NATIONMAX FROM (SELECT avg(S_acctbal) as S_acct FROM Supplier GROUP BY S_nationkey ORDER BY S_nationkey) tmp"
+                    ,"SELECT SUM(L_extendedprice*L_discount) AS REVENUE FROM Lineitem WHERE L_shipdate >= '1994-01-01' AND L_shipdate < cast('1995-01-01' as date) AND L_discount between .06 - 0.01 AND .06 + 0.01 AND L_quantity < 24" -- Q6
+                    ,"SELECT C_custkey, C_name, SUM(L_extendedprice*(1-L_discount)) AS REVENUE, C_acctbal, N_name, C_address, C_phone, C_comment FROM Customer, Orders, Lineitem, Nation WHERE C_custkey = O_custkey AND L_orderkey = O_orderkey AND O_orderdate>= '1993-10-01' AND O_orderdate < cast('1994-01-01' as date) AND L_returnflag = 'R' AND C_nationkey = N_nationkey GROUP BY C_custkey, C_name, C_acctbal, C_phone, N_name, C_address, C_comment ORDER BY REVENUE DESC LIMIT 20" -- Q10
+                    ,"SELECT CNTRYCODE, COUNT(*) AS NUMCUST, SUM(C_acctbal) AS TOTACCTBAL FROM (SELECT SUBSTRING(C_phone,1,2) AS CNTRYCODE, C_acctbal FROM Customer WHERE SUBSTRING(C_phone,1,2) IN ('13', '31', '23', '29', '30', '18', '17') AND C_acctbal > (SELECT AVG(C_acctbal) FROM Customer WHERE C_acctbal > 0.00 AND SUBSTRING(C_phone,1,2) IN ('13', '31', '23', '29', '30', '18', '17')) AND NOT EXISTS ( SELECT * FROM Orders WHERE O_custkey = C_custkey)) AS CUSTSALE GROUP BY CNTRYCODE ORDER BY CNTRYCODE" -- Q22
+                    ,"SELECT C.c_custkey, C.c_name, SUM(L.l_extendedprice*(1-L.l_discount)) AS REVENUE, C.c_acctbal, N.n_name, C.c_address, C.c_phone, C.c_comment FROM Customer C, Orders O, Lineitem L, Nation N WHERE C.c_custkey = O.o_custkey AND L.l_orderkey = O.o_orderkey AND O.o_orderdate>= '1993-10-01' AND O.o_orderdate < cast('1994-01-01' as date) AND L.l_returnflag = 'R' AND C.c_nationkey = N.n_nationkey GROUP BY C.c_custkey, C.c_name, C.c_acctbal, C.c_phone, N.n_name, C.c_address, C.c_comment ORDER BY REVENUE DESC LIMIT 20" -- Q10, with tables renamed
+                    ,"SELECT C_name, N_nationkey FROM Customer, Nation WHERE C_custkey = N_nationkey"
+                    ,"SELECT C_name, O_orderkey FROM Customer, Orders WHERE O_totalprice = C_custkey "
                    ]
 
     -- test pretty print
@@ -104,6 +97,26 @@ runTestQueries spec setting = do
     putStrLn "\n\n"
     return ()
 
+runTPCHQueries :: MtSchemaSpec -> MtSetting -> IO ()
+runTPCHQueries spec setting = do
+    let queries = ["SELECT L_returnflag, L_linestatus, SUM(L_quantity) AS SUM_QTY, SUM(L_extendedprice) AS SUM_BASE_PRICE, SUM(L_extendedprice*(1-L_discount)) AS SUM_DISC_PRICE, SUM(L_extendedprice*(1-L_discount)*(1+L_tax)) AS SUM_CHARGE, AVG(L_quantity) AS AVG_QTY, AVG(L_extendedprice) AS AVG_PRICE, AVG(L_discount) AS AVG_DISC, COUNT(*) AS COUNT_ORDER FROM Lineitem WHERE L_shipdate <= cast('1998-09-02' as date) GROUP BY L_returnflag, L_linestatus ORDER BY L_returnflag,L_linestatus" -- Q01
+                    ,"SELECT SUM(L_extendedprice*L_discount) AS REVENUE FROM Lineitem WHERE L_shipdate >= '1994-01-01' AND L_shipdate < cast('1995-01-01' as date) AND L_discount between .06 - 0.01 AND .06 + 0.01 AND L_quantity < 24" -- Q6
+                    ,"SELECT C_custkey, C_name, SUM(L_extendedprice*(1-L_discount)) AS REVENUE, C_acctbal, N_name, C_address, C_phone, C_comment FROM Customer, Orders, Lineitem, Nation WHERE C_custkey = O_custkey AND L_orderkey = O_orderkey AND O_orderdate>= '1993-10-01' AND O_orderdate < cast('1994-01-01' as date) AND L_returnflag = 'R' AND C_nationkey = N_nationkey GROUP BY C_custkey, C_name, C_acctbal, C_phone, N_name, C_address, C_comment ORDER BY REVENUE DESC LIMIT 20" -- Q10
+                    ,"SELECT CNTRYCODE, COUNT(*) AS NUMCUST, SUM(C_acctbal) AS TOTACCTBAL FROM (SELECT SUBSTRING(C_phone,1,2) AS CNTRYCODE, C_acctbal FROM Customer WHERE SUBSTRING(C_phone,1,2) IN ('13', '31', '23', '29', '30', '18', '17') AND C_acctbal > (SELECT AVG(C_acctbal) FROM Customer WHERE C_acctbal > 0.00 AND SUBSTRING(C_phone,1,2) IN ('13', '31', '23', '29', '30', '18', '17')) AND NOT EXISTS ( SELECT * FROM Orders WHERE O_custkey = C_custkey)) AS CUSTSALE GROUP BY CNTRYCODE ORDER BY CNTRYCODE" -- Q22
+                   ]
+
+    -- rewrite
+    mapM_ (\query -> do
+        putStrLn "\n====================================================\n"
+        putStrLn $ query ++ " rewrites to:\n"
+        let rewrittenQuery = mtRewrite spec setting query
+        putStrLn (mtPrettyPrint rewrittenQuery)
+        )
+        queries
+
+    putStrLn "\n\n"
+    return ()
+
 sessionLoop :: MtSchemaSpec -> MtSetting -> IO ()
 sessionLoop spec setting = do
     putStrLn ">> Please write a query you want to rewrite or type 'logout'/'l' to logout or 'test' to run a set of test queries."
@@ -116,9 +129,14 @@ sessionLoop spec setting = do
                     runTestQueries spec setting
                     sessionLoop spec setting
                 else do
-                    putStrLn $ "The query parses to:\n" ++ mtPrettyPrint (mtParse line)
-                    putStrLn $ "\nIts rewritten form is:\n  " ++ (mtPrettyPrint (mtRewrite spec setting line))
-                    sessionLoop spec setting
+                    if line == "tpch"
+                        then do
+                            runTPCHQueries spec setting
+                            sessionLoop spec setting
+                        else do
+                            putStrLn $ "The query parses to:\n" ++ mtPrettyPrint (mtParse line)
+                            putStrLn $ "\nIts rewritten form is:\n  " ++ (mtPrettyPrint (mtRewrite spec setting line))
+                            sessionLoop spec setting
 
 mainLoop :: MtSchemaSpec -> IO ()
 mainLoop spec = do
